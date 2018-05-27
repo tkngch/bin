ebde5de9524b92b1ad4c6a4581df34532c684a76
Takao Noguchi <tkngch@runbox.com>
2018-05-27 14:38:19 +0100

    update

--- install_fonts.sh
+++ install_fonts.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/sh
 
-# pacman -Ss otf | /usr/bin/awk '{print $1}' > /tmp/f
-
 pacman -Ss otf | egrep '^\w' | /usr/bin/awk '{print $1}' > /tmp/fonts
 pacman -Ss ttf | egrep '^\w' | /usr/bin/awk '{print $1}' >> /tmp/fonts
-
-# awk 'NR%2 != 0' /tmp/f > /tmp/fonts
-# pacman -S `cat /tmp/fonts`
+pacman -S `cat /tmp/fonts`
 # rm -f /tmp/f /tmp/fonts
