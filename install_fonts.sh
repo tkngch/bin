a4dc29c50c1e1daf23711e088a191b1cb690a7ae
Takao Noguchi <tkngch@runbox.com>
2020-05-11 09:04:38 +0100

    refactor

--- install_fonts.sh
+++ install_fonts.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/sh
 
-pacman -Ss otf | egrep '^\w' | /usr/bin/awk '{print $1}' > /tmp/fonts
-pacman -Ss ttf | egrep '^\w' | /usr/bin/awk '{print $1}' >> /tmp/fonts
-pacman -S `cat /tmp/fonts`
+pacman -Ss otf | grep -E '^\w' | /usr/bin/awk '{print $1}' > /tmp/fonts
+pacman -Ss ttf | grep -E '^\w' | /usr/bin/awk '{print $1}' >> /tmp/fonts
+pacman -S $(cat /tmp/fonts)
 # rm -f /tmp/f /tmp/fonts
